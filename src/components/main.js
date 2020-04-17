8d1f909e42a3d55994c46c78c58c7e81f9521ad7
Jan Gosmann <jan@hyper-world.de>
2020-04-17 21:18:59 +0200

    Remove unnecessary styling div

--- src/components/main.js
+++ src/components/main.js
@@ -4,22 +4,16 @@ import PropTypes from "prop-types"
 export const mainWidth = 664
 
 const Main = ({ children, title }) => (
-  <div
+  <main
     css={{
-      flexGrow: 1,
+      padding: "32px 32px 64px",
+      boxSizing: "border-box",
+      maxWidth: mainWidth,
     }}
   >
-    <main
-      css={{
-        padding: "32px 32px 64px",
-        boxSizing: "border-box",
-        maxWidth: mainWidth,
-      }}
-    >
-      <h1>{title}</h1>
-      {children}
-    </main>
-  </div>
+    <h1>{title}</h1>
+    {children}
+  </main>
 )
 
 Main.propTypes = {
